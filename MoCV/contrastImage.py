040c46a7cd5b36621634560c98c0d557c781dd17
quocdat32461997 <quocdat32461997@yahoo.com>
2020-01-19 14:47:52 -0600

    Update equalizer

--- MoCV/contrastImage.py
+++ MoCV/contrastImage.py
@@ -1,6 +1,7 @@
 #contrastImage.py
 
 from . import histogram
+import numpy as np
 import cv2
 
 """
@@ -16,16 +17,26 @@ def upcontrastImage(image):
 	#compute hist of each channel and equalize
 	equalized_hists = []
 	
+	#convert gray-scale to 3-dim image
+	image = np.expand_dims(image, axis = 2) 
+	
+	#equalize histogram to increase contrast
 	for channel_idx in range(image.shape[2]):
 		#calculate histogram
 		hist, pixels = histogram.calcHist(image[:,:,channel_idx])
 		#equalize histogram
 		equalized_hists.append(histogram.equalizeHist(hist, pixels))
 
-	#contrsuct new channel
-	for channel in equalized_hists:
-		for color, pixels in channel.items():
-			for pixel in pixels:
-				image[pixel.x, pixel.y, channel] = color	
 
-	return image
+	new_image = np.zeros(shape = image.shape)
+	#contrsuct new image
+	for channel_idx in range(len(equalized_hists)):
+		channel = equalized_hists[channel_idx]
+		for color in channel.keys():
+			print(color)
+			pixels = channel[color]
+			print(len(pixels))
+			for pixel in pixels:
+				new_image[pixel.x, pixel.y, channel_idx] = color	
+
+	return new_image 
