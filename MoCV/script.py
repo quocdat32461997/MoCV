eafb896391c76b09a78857f2ab2dd67f890ed951
quocdat32461997 <quocdat32461997@yahoo.com>
2020-02-02 22:38:30 -0600

    Remove scripy.py

--- MoCV/script.py
+++
@@ -1,9 +0,0 @@
-def avg(marks):
-    assert len(marks) != 0,"List is empty."
-    return sum(marks)/len(marks)
-
-mark2 = [55,88,78,90,79]
-print("Average of mark2:",avg(mark2))
-
-mark1 = []
-print("Average of mark1:",avg(mark1))
