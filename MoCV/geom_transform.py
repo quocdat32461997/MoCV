a05e8154072589bafe856c57dc694ccd2296049b
quocdat32461997 <quocdat32461997@yahoo.com>
2020-02-06 17:57:35 -0600

    Add scaling by bilinear_interpolation

--- MoCV/geom_transform.py
+++ MoCV/geom_transform.py
@@ -6,7 +6,7 @@ Last updated: 02/02/2020
 
 #import dependencies
 import numpy as np
-__all__ = ['translation', 'nn']
+__all__ = ['translation', 'nn', 'linear_interpolate']
 
 """
 translation - function to move the image with alpha and beta spaces
@@ -35,41 +35,91 @@ def translation(image, alpha, beta):
 					new_img[row, col] = image[row + alpha, col + beta] 
 
 	return new_img
-"""		end of translation		"""
+	"""		end of translation		"""
+
 
 """
 nn - function nearest neigbor to map pixels of scaled images to the original images for image interpolation. By default, assume that scale around (0, 0)
 Parameters:
 	orgi_img	I/P	original image input
 	scaled_img	I/P	scaled image input - 2d array of mapped pixels
+	h_ratio		I/P	scaling ratio for height
+	w_ratio		I/P	scaling ratio for width
 			O/P	color-filled image
 """
-def nn(origi_img, scaled_img):
+def nn(origi_img, scaled_img, h_ratio, w_ratio):
 	"""
 		nearest neighbor - a linear interpolation to fill pixels of scaled images by picking the neartest pixels 
 	"""
 
-	#get height and width of scaled_img and origin_img
+	#get height and width of scaled_img and origi_img
 	height = scaled_img.shape[0]
 	h = origi_img.shape[0]
 	width = scaled_img.shape[1]
 	w = origi_img.shape[1]
 	
-	#get scaling ratio of width and height
-	h_ratio = origi_img.shape[0] / scaled_img.shape[0]
-	w_ratio = origi_img.shape[1] / scaled_img.shape[1]
-	
 	#visit each pixel and map back to original image
 	for row in range(height):
+		x = round(row / h_ratio) 
+		if not x in range(h): #if out-of-bound, fill w/ zeros
+			continue
+
 		for col in range(width):
-			x = round(row * h_ratio) #get scaled_pixels
-			y = round(col * w_ratio)
+			y = round(col / w_ratio)
 
 			#map to the nearest pixels
-			if not x in range(h) or not y in range(w): #if out-of-bound, fill with zero 
-				scaled_img[row, col] = 0
+			if not y in range(w): #if out-of-bound, fill with zero 
+				continue
 			else:
 				scaled_img[row, col] = origi_img[x, y]
 	return scaled_img
-"""		end of nn		"""
-	
+	"""		end of nn		"""
+
+
+"""
+linear_interpolate - function to bilinear interpolatation trasformation
+Parameters:
+	origi_img	I/P	image input
+	scaled_img	I/P	scaled image input - 2d array of mapped pixels
+	h_ratio		I/P	scaling ratio for height
+	w_ratio		I/P	scaling ratio for width
+			O/P	transformed image
+"""
+def linear_interpolate(origi_img, scaled_img, h_ratio, w_ratio):
+	#get height and width of scaled_img and origi_img
+	height = scaled_img.shape[0]
+	width = scaled_img.shape[1]
+	h = origi_img.shape[0]
+	w = origi_img.shape[1]
+
+	#visit each pixel and map back to original image
+	#extra variables are pre-computed in order to prevent re-computing within for loops
+	for row in range(height):
+		#get x, floor and ceil of x, compute weight b
+		x = row / h_ratio
+		fx = np.int(np.floor(x))
+		cx = np.int(np.ceil(x))
+
+		#if out-of-bound, fill w/ zero
+		if not fx in range(h) or not cx in range(h):
+			continue
+		#compute weight
+		b = x - fx
+		nb = 1 - b
+			
+		for col in range(width):
+			#get y, floor and ceil of y, and compute weight a
+			y = col / w_ratio
+			fy = np.int(np.floor(y))
+			cy = np.int(np.ceil(y))
+			
+			#compute transformed pixels
+			if fy in range(w) and cy in range(w): #if out-of-bound, fill with zeros 
+				#compute weight
+				a = y - fy
+				na = 1 - a
+				scaled_img[row, col] = na * nb * origi_img[fx, fy] + na * b * origi_img[fx, cy] + a * nb * origi_img[cx, fy] + a * b * origi_img[cx, cy]
+			else:
+				pass
+	return scaled_img
+	"""		linear_interpolate		"""
--- MoCV/img_func.py
+++ MoCV/img_func.py
@@ -7,7 +7,7 @@ Last udpated: Feb. 2, 2020
 #import dependencies
 import numpy as np
 from . import geom_transform as gtf
-__all__ = ['shift', 'scale_nn']
+__all__ = ['shift', 'scale_nn', 'scale_lp']
 
 """
 shift - function to shift images
@@ -35,16 +35,48 @@ Parameters:
 def scale_nn(img, h_ratio, w_ratio = 0):
 	#Assume to scale around (0, 0)
 	assert(h_ratio != 0), "Ratio cannot be zero or 0" #scaling ratio cannot be zero or 0
+	h_ratio = round(h_ratio)
+	w_ratio = round(w_ratio)
 	if w_ratio == 0: #only one given scaling ratio
 		w_ratio = h_ratio
 
 	#get width and height after scaling
-	scaled_width = img.shape[1] * round(w_ratio) #scaling ratio always integer as image size not decimal
-	scaled_height = img.shape[0] * round(h_ratio)
+	scaled_w = img.shape[1] * w_ratio #scaling ratio always integer as image size not decimal
+	scaled_h = img.shape[0] * h_ratio
+
+	#create empty image after scaling
+	scaled_img = np.array([[0] * scaled_w] * scaled_h)
+
+	#scale by nearest neighbor
+	scaled_img = gtf.nn(img, scaled_img, h_ratio, w_ratio)
+	return scaled_img
+	"""		end of scaled_nn		"""
+
+"""
+scale_lp - function to do geometric trasnformation by bilinear transformation
+Parameters:
+	img		I/P	image input
+	h_ratio		I/P	ratio for scaling height
+	w_ratio		I/P	ratio for scaling width
+	saled_img	O/P	scaled image output
+"""
+def scale_lp(img, h_ratio, w_ratio = 0):
+	#Assume to scale around (0, 0)
+	assert(h_ratio != 0), "Ratio cannot be zero or 0" #scaling ratio cannot be zero or 0
+	if w_ratio == 0: #only one given scaling ratio
+		w_ratio = h_ratio
+	h_ratio = round(h_ratio)
+	w_ratio = round(w_ratio)
+
+	#get width and height after scaling
+	scaled_width = img.shape[1] * w_ratio #scaling ratio always integer as image size not decimal
+	scaled_height = img.shape[0] * h_ratio
 
 	#create empty image after scaling
 	scaled_img = np.array([[0] * scaled_width] * scaled_height)
 
-	scaled_img = gtf.nn(img, scaled_img)
+	#biliner interpolate
+	scaled_img = gtf.linear_interpolate(img, scaled_img, h_ratio, w_ratio)
+
 	return scaled_img
-	"""		end of scaled_img		"""
+	"""		end of scale_lp		"""
